2219bae428a5131faca6fc4b030ea8bd33c68c12
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2021-11-04 20:08:35 +0000

    first commit

---
+++ wp-content/uploads/custom-css-js/8261.js
@@ -0,0 +1,29 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+ 
+
+
+jQuery(document).ready(function( $ ){
+  	
+  jQuery('.content-scroll-bottom').on( 'click', () => {
+    console.log('evento clic');
+    jQuery('#entradaM23').animate(
+    	{ scrollTop: jQuery('#mainContentWallM23Good008 .vc_tta-container').offset().top -80 }, 500
+    );
+  })   
+  
+});
+
+function scrollBottom() {
+  
+  
+  
+  
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 500;
+  //window.scrollBy(0, 500);
+}
+
+
+</script>
+<!-- end Simple Custom CSS and JS -->
